b8e325cba900167cc559cb77c61884ccf10cd044
Harshit3929 <harshit.d.singhparmar@gmail.com>
2026-03-23 14:24:51 +0530

    Update Neural_Network.py

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,22 +1,21 @@
 class Neuron:
 
-    def __init__(self, nin):
-        self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
-        self.b = Value(random.uniform(-1, 1))
+    def __init__ (self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(0.0)  # init bias to 0 to avoid dead neurons at start
 
-    def __call__(self, x, activation="relu"):
-
-        act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
-        if activation == "relu":
+    def __call__ (self, x, activation='relu'):
+        act = sum((wi*xi for wi,xi in zip(self.w,x)), self.b)
+        if activation == 'relu':
             return act.relu()
-        elif activation == "sigmoid":
+        elif activation == 'sigmoid':
             return act.sigmoid()
-        elif activation == "tanh":
+        elif activation == 'tanh':
             return act.tanh()
-        else:
+        else: 
             return act
 
-    def parameters(self):
+    def parameters (self):
         return self.w + [self.b]
 
 
@@ -25,8 +24,8 @@ class Layer:
     def __init__(self, nin, nout):
         self.neuron = [Neuron(nin) for _ in range(nout)]
 
-    def __call__(self, x, activation="relu"):
-        outs = [n(x) for n in self.neuron]
+    def __call__(self, x, activation='relu'):  
+        outs = [n(x, activation) for n in self.neuron]  
         return outs[0] if len(outs) == 1 else outs
 
     def parameters(self):
@@ -35,13 +34,14 @@ class Layer:
 
 class MLP:
 
-    def __init__(self, nin, nouts):
+    def __init__ (self, nin, nouts):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
 
-    def __call__(self, x, activation="relu"):
-        for layers in self.layers:
-            x = layers(x)
+    def __call__(self, x):
+        for i, layer in enumerate(self.layers):
+            activation = 'linear' if i == len(self.layers) - 1 else 'tanh'
+            x = layer(x, activation)
         return x
 
     def parameters(self):
